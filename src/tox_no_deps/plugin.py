0091a99f6b119a1f54f69a5f557d145eeca4deb3
Stanislav Levin <slev@altlinux.org>
2021-03-27 17:39:53 +0300

    Display only not empty skipped deps and extras
    Signed-off-by: Stanislav Levin <slev@altlinux.org>

--- src/tox_no_deps/plugin.py
+++ src/tox_no_deps/plugin.py
@@ -19,14 +19,23 @@ def tox_configure(config):
     if not config.option.no_deps:
         return
 
-    for _, envconfig in config.envconfigs.items():
-        reporter.verbosity1(
-            f"no-deps plugin: deps: '{envconfig.deps}' will be skipped for "
-            f"'{envconfig.envname}'"
-        )
-        envconfig.deps = []
-        reporter.verbosity1(
-            f"no-deps plugin: extras: '{envconfig.extras}' will be skipped for "
-            f"'{envconfig.envname}'"
-        )
-        envconfig.extras = []
+    for envconfig in config.envconfigs.values():
+        if not isinstance(envconfig.deps, list):
+            raise TypeError("Not supported deps type", envconfig.deps)
+
+        if envconfig.deps:
+            reporter.verbosity1(
+                f"no-deps plugin: deps: '{envconfig.deps}' will be skipped for"
+                f" '{envconfig.envname}'"
+            )
+            envconfig.deps = []
+
+        if not isinstance(envconfig.extras, list):
+            raise TypeError("Not supported extras type", envconfig.extras)
+
+        if envconfig.extras:
+            reporter.verbosity1(
+                f"no-deps plugin: extras: '{envconfig.extras}' will be skipped"
+                f" for '{envconfig.envname}'"
+            )
+            envconfig.extras = []
